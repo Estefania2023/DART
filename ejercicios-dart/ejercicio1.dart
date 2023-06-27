871eea74cdfe7f48175831be6fa4c8316c1ea568
Estefania <estefaniajimenezpana@gmail.com>
2023-06-27 15:25:09 -0500

    ejercicio tipo de dato entero

--- ejercicios-dart/ejercicio1.dart
+++ ejercicios-dart/ejercicio1.dart
@@ -1,3 +1,8 @@
 void main() {
-  print('Hello, World!');
+  //print('Hello, World!');
+
+  int num1 = 2;
+  double num2 = 2.5;
+  double suma = num1 + num2;
+  print('la suma de dos numeros es:$suma');
 }
